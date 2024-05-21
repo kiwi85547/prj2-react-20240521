ac66d1a163a452c17a06ce911ee24df3c7032f61
Hwayeong <joid1004@naver.com>
2024-05-21 11:24:34 +0900

    Navbar.jsx 작성

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,11 +1,14 @@
 import { Box } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
 import React from "react";
+import { Navbar } from "./Component/Navbar.jsx";
 
 export function Home() {
   return (
     <Box>
-      <Box>navbar</Box>
+      <Box>
+        <Navbar />
+      </Box>
       <Box>
         <Outlet />
       </Box>
