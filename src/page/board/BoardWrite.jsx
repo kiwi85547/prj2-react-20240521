59b33bb4cd20ea2e9852eff5d3d78b48c15bb231
Hwayeong <joid1004@naver.com>
2024-05-21 12:20:08 +0900

    글 등록 성공시 toast 출력, HOME으로 이동

--- src/page/board/BoardWrite.jsx
+++ src/page/board/BoardWrite.jsx
@@ -1,19 +1,40 @@
-import { Box, Button, FormControl, FormLabel, Input } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  useToast,
+} from "@chakra-ui/react";
 import React, { useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export function BoardWrite() {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [writer, setWriter] = useState("");
+  const toast = useToast();
+  const navigate = useNavigate();
 
   function handleSaveClick() {
-    axios.post("/api/board/add", {
-      // property 명과 값이 같으면 하나만 써도 됨 title:title
-      title,
-      content,
-      writer,
-    });
+    axios
+      .post("/api/board/add", {
+        // property 명과 값이 같으면 하나만 써도 됨 title:title
+        title,
+        content,
+        writer,
+      })
+      .then(() => {
+        toast({
+          description: "새 글이 등록되었습니다.",
+          status: "success",
+          position: "top",
+        });
+        navigate("/");
+      })
+      .catch()
+      .finally();
   }
 
   return (
