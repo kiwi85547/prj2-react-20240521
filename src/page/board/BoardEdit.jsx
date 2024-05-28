9a8a764cbd4207843b8f762c47faeed920ca441a
Hwayeong <joid1004@naver.com>
2024-05-28 15:40:49 +0900

    지울 파일 목록 전송

--- src/page/board/BoardEdit.jsx
+++ src/page/board/BoardEdit.jsx
@@ -6,6 +6,7 @@ import {
   Button,
   Flex,
   FormControl,
+  FormHelperText,
   FormLabel,
   Image,
   Input,
@@ -29,6 +30,7 @@ export function BoardEdit() {
   const { id } = useParams();
   const [board, setBoard] = useState(null);
   const [removeFileList, setRemoveFileList] = useState([]);
+  const [addFileList, setAddFileList] = useState([]);
   const toast = useToast();
   const navigate = useNavigate();
   const { isOpen, onClose, onOpen } = useDisclosure();
@@ -71,6 +73,12 @@ export function BoardEdit() {
 
   if (board === null) {
     return <Spinner />;
+  }
+
+  // 추가 파일 선택하면 목록 보이기
+  const fileNameList = [];
+  for (let addFile of addFileList) {
+    fileNameList.push(<li>{addFile.name}</li>);
   }
 
   function handleRemoveSwitchChange(name, checked) {
@@ -138,6 +146,24 @@ export function BoardEdit() {
                 </Box>
               ))}
           </Box>
+        </Box>
+
+        <Box>
+          <FormControl>
+            <FormLabel>파일</FormLabel>
+            <Input
+              multiple
+              type="file"
+              accept="image/*"
+              onChange={(e) => setAddFileList(e.target.files)}
+            />
+            <FormHelperText>
+              총 용량은 10MB, 한 파일은 1MB를 초과할 수 없습니다.
+            </FormHelperText>
+          </FormControl>
+        </Box>
+        <Box>
+          <ul>{fileNameList}</ul>
         </Box>
 
         <Box>
