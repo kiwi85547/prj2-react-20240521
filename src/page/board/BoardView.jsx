1cf9ba0572b6cf13ac97383e189795bcff37b996
Hwayeong <joid1004@naver.com>
2024-05-28 11:35:44 +0900

    첨부된 이미지 출력

--- src/page/board/BoardView.jsx
+++ src/page/board/BoardView.jsx
@@ -6,6 +6,7 @@ import {
   Button,
   FormControl,
   FormLabel,
+  Image,
   Input,
   Modal,
   ModalBody,
@@ -91,6 +92,17 @@ export function BoardView() {
             <FormLabel>본문</FormLabel>
             <Textarea value={board.content} readOnly />
           </FormControl>
+        </Box>
+
+        <Box>
+          <Box>
+            {board.imageSrcList &&
+              board.imageSrcList.map((src) => (
+                <Box key={src}>
+                  <Image src={src} />
+                </Box>
+              ))}
+          </Box>
         </Box>
 
         <Box>
