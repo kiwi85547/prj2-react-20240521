6ea01b22ab203fac8970a865f9f06b0d6e141dd1
Hwayeong <joid1004@naver.com>
2024-05-30 15:42:10 +0900

    댓글 지우기(에러 해결). 댓글 삭제 후 새로고침 하지 않아도 바로 목록에게 삭제되게 만들기 연습 시작

--- src/component/comment/CommentItem.jsx
+++ src/component/comment/CommentItem.jsx
@@ -1,13 +1,35 @@
-import { Box, Flex, Spacer } from "@chakra-ui/react";
+import { Box, Button, Flex, Spacer } from "@chakra-ui/react";
+import { faTrashCan } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from "axios";
 
-export function CommentItem(props) {
-  return;
-  <Box key={comment.id} border={"1px solid black"} my={3}>
-    <Flex>
-      <Box>{comment.nickName}</Box>
-      <Spacer />
-      <Box>{comment.inserted}</Box>
-      <Box>{comment.comment}</Box>
-    </Flex>
-  </Box>;
+export function CommentItem({ comment }) {
+  function handleRemoveClick() {
+    axios
+      .delete(`/api/comment/remove`, {
+        data: { id: comment.id },
+      })
+      .then((res) => {})
+      .catch((err) => {})
+      .finally(() => {});
+  }
+
+  return (
+    <Box border={"1px solid black"} my={3}>
+      <Flex>
+        <Box>{comment.nickName}</Box>
+        <Spacer />
+        <Box>{comment.inserted}</Box>
+      </Flex>
+      <Flex>
+        <Box>{comment.comment}</Box>
+        <Spacer />
+        <Box>
+          <Button colorScheme="red" onClick={handleRemoveClick}>
+            <FontAwesomeIcon icon={faTrashCan} />
+          </Button>
+        </Box>
+      </Flex>
+    </Box>
+  );
 }
