f18caa284ad4f4b37497c7821795041d61205a05
Hwayeong <joid1004@naver.com>
2024-05-30 11:17:39 +0900

    댓글 목록

--- src/component/comment/CommentList.jsx
+++ src/component/comment/CommentList.jsx
@@ -1,3 +1,31 @@
-export function CommentList() {
-  return null;
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Box, Flex, Spacer } from "@chakra-ui/react";
+
+export function CommentList({ boardId }) {
+  const [commentList, setCommentList] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`/api/comment/list/${boardId}`)
+      .then((res) => setCommentList(res.data))
+      .catch((err) => console.log(err))
+      .finally();
+  }, []);
+  if (commentList.length === 0) {
+    return <Box>댓글이 없습니다.</Box>;
+  }
+  return (
+    <Box>
+      {commentList.map((comment) => (
+        <Box key={comment.id} border={"1px solid black"} my={3}>
+          <Flex>
+            <Box>{comment.memberId}</Box>
+            <Spacer />
+            <Box>{comment.inserted}</Box>
+            <Box>{comment.comment}</Box>
+          </Flex>
+        </Box>
+      ))}
+    </Box>
+  );
 }
