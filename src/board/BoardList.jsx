59b47c442dcbcb9b23b86b43d734546a92b02a7c
Hwayeong <joid1004@naver.com>
2024-06-02 20:11:42 +0900

    게시판 리스트

--- src/board/BoardList.jsx
+++ src/board/BoardList.jsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { Box } from "@chakra-ui/react";
+import { Box, Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
 
 export function BoardList() {
-  const [boardList, setBoardList] = useState({});
+  const [boardList, setBoardList] = useState([]);
 
   useEffect(() => {
     axios
@@ -15,5 +15,31 @@ export function BoardList() {
       .finally();
   }, []);
 
-  return <Box>응답되었습니다.</Box>;
+  return (
+    <Box>
+      <Box m={5} fontSize={"1.8rem"}>
+        게시판
+      </Box>
+      <Box>
+        <Table>
+          <Thead>
+            <Tr>
+              <Th>#</Th>
+              <Th>제목</Th>
+              <Th>작성자</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {boardList.map((board) => (
+              <Tr key={board.id}>
+                <Td>{board.id}</Td>
+                <Td>{board.title}</Td>
+                <Td>{board.memberId}</Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </Box>
+    </Box>
+  );
 }
