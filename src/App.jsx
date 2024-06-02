c6e9c89043db41b1e9bfabd15e6308bf325cc574
Hwayeong <joid1004@naver.com>
2024-06-02 12:15:20 +0900

    라우터 및 컴포넌트 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Home } from "./Home.jsx";
+import { BoardList } from "./board/BoardList.jsx";
+import { BoardWrite } from "./board/BoardWrite.jsx";
+import { BoardView } from "./board/BoardView.jsx";
+import { BoardEdit } from "./board/BoardEdit.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    children: [
+      {
+        index: true,
+        element: <BoardList />,
+      },
+      { path: "write", element: <BoardWrite /> },
+      { path: "board/:id", element: <BoardView /> },
+      { path: "edit/:id", element: <BoardEdit /> },
+    ],
+  },
+]);
 
 function App(props) {
-  return <div></div>;
+  return;
+  <ChakraProvider>
+    <RouterProvider router={router} />
+  </ChakraProvider>;
 }
 
 export default App;
